344aa011e4b79ad8b82708905a895caf3fc068dc
raniagh <rghzaiel@ggn-group.com>
2024-10-01 11:36:22 +0100

    design responsive menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,31 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <div></div>
+    <div className='h-screen flex'>
+      {/* LEFT */}
+      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4'>
+        <Link
+          href='/'
+          className='flex gap-2 items-center justify-center lg:justify-start'
+        >
+          <Image src='/logo.png' alt='logo' width={32} height={32} />
+          <span className='hidden lg:block'>School</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll '>
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
