b4eeed48edd604443fd0514308c209c38256ccc2
raniagh <rghzaiel@ggn-group.com>
2024-10-09 14:52:32 +0100

    protect routes with roles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
+  const role = user?.publicMetadata.role as string;
+
   return (
     <div className='flex items-center justify-between p-4'>
       {/* SEARCH BAR */}
@@ -31,7 +35,7 @@ const Navbar = () => {
         </div>
         <div className='flex flex-col'>
           <span className='text-xs leading-3 font-medium'>John Doe</span>
-          <span className='text-[10px] text-gray-500 text-right'>Admin</span>
+          <span className='text-[10px] text-gray-500 text-right'>{role}</span>
         </div>
         {/*  <Image
           src='/avatar.png'
